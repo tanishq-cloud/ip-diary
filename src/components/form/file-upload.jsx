5df585f8db8a37a303ae2d5fc6b1a30a00ecc69d
Tanishq Kumar <tanishqkumar@protonmail.com>
2025-02-11 22:24:39 +0530

    added wiki hyperlinks

--- src/components/form/file-upload.jsx
+++ src/components/form/file-upload.jsx
@@ -154,25 +154,25 @@ const FileUpload = ({ onDataProcessed }) => {
 
       {activeTab === "upload" && (
         <div>
-        <input
-          type="file"
-          accept=".xlsx, .xls, .csv"
-          onChange={handleFileUpload}
-          className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-        />
+          <input
+            type="file"
+            accept=".xlsx, .xls, .csv"
+            onChange={handleFileUpload}
+            className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+          />
 
-        <div className="mt-2 text-sm text-gray-600">
-      <span>Need help formatting your CSV file? </span>
-      <a
-        href="https://github.com/tanishq-cloud/ip-diary/wiki/Task-Format-(CSV-File)"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        Click here
-      </a>
-    </div>
-    </div>
+          <div className="mt-2 text-sm text-gray-600">
+            <span>Need help formatting your CSV file? </span>
+            <a
+              href="https://github.com/tanishq-cloud/ip-diary/wiki/Task-Format-(CSV-File)"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Click here
+            </a>
+          </div>
+        </div>
       )}
 
       {activeTab === "link" && (
